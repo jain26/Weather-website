deaf66794807a679fab2f7c28cc0d5c56e2976f3
Ayush Jain <ayush.j26@gmail.com>
2020-07-09 12:01:39 +0530

    forecast and about

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude,longitude, callback) => {
             callback('Unable to find location',undefined)
         }
         else {
-            callback(undefined,body.current.weather_descriptions[0] + ', The temperature is currently ' + body.current.temperature + ' and it feels like ' + body.current.feelslike)
+            callback(undefined,body.current.weather_descriptions[0] + ', The temperature is currently ' + body.current.temperature + ' and it feels like ' + body.current.feelslike+'.The Humidity is '+body.current.humidity+' percent.')
         }
     })
 }
